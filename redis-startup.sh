5cd77b5aaa73e67bef31369cdce94a485ef2ceb5
Alexey Kolosov <alexey.kolosov@gmail.com>
2014-12-09 12:28:04 +0500

    FIX

--- redis-startup.sh
+++ redis-startup.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
-sysctl vm.overcommit_memory=1
-
 if [ -n "$REDIS_MASTER_SERVER" ]; then
-	exec /usr/local/bin/redis-server /etc/redis/redis.conf $@
+	/usr/local/bin/redis-server /etc/redis/redis.conf $@
 fi
 
 if [ -n "$REDIS_CUSTOM_MASTER_SERVER" ]; then
-	exec /usr/local/bin/redis-server $REDIS_CUSTOM_MASTER_SERVER $@
+	/usr/local/bin/redis-server $REDIS_CUSTOM_MASTER_SERVER $@
 fi
 
 if [ -n "$REDIS_SLAVE_SERVER" ]; then
-	exec /usr/local/bin/redis-server /etc/redis/redis.conf --slaveof $REDIS_SLAVE_SERVER $@
+	/usr/local/bin/redis-server /etc/redis/redis.conf --slaveof $REDIS_SLAVE_SERVER $@
 fi
 
 if [ -n "$REDIS_CUSTOM_SLAVE_SERVER" ]; then
 	if [ -n "$REDIS_SLAVE_SERVER_FOR" ]; then
-		exec /usr/local/bin/redis-server $REDIS_CUSTOM_SLAVE_SERVER --slaveof $REDIS_SLAVE_SERVER_FOR $@
+		/usr/local/bin/redis-server $REDIS_CUSTOM_SLAVE_SERVER --slaveof $REDIS_SLAVE_SERVER_FOR $@
 	else
-		exec /usr/local/bin/redis-server $REDIS_CUSTOM_SLAVE_SERVER $@
+		/usr/local/bin/redis-server $REDIS_CUSTOM_SLAVE_SERVER $@
 	fi
 fi
 
 if [ -n "$REDIS_SENTNEL_SERVER" ]; then
-	exec /usr/local/bin/redis-sentinel /etc/redis/sentinel.conf $@
+	/usr/local/bin/redis-sentinel /etc/redis/sentinel.conf $@
 fi
 
 if [ -n "$REDIS_CUSTOM_SENTNEL_SERVER" ]; then
-	exec /usr/local/bin/redis-sentinel $REDIS_CUSTOM_SENTNEL_SERVER $@ 
+	/usr/local/bin/redis-sentinel $REDIS_CUSTOM_SENTNEL_SERVER $@ 
 fi
 
 if [ -n "$REDIS_CLIENT" ]; then
-	exec /bin/bash $@
+	/bin/bash $@
 fi
 
