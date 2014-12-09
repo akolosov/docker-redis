10e0891ec9bcfec165c2594373e93664571d7ff9
Alexey Kolosov <alexey.kolosov@gmail.com>
2014-12-09 10:48:31 +0500

    FIX

--- Dockerfile
+++ Dockerfile
@@ -21,18 +21,21 @@ RUN \
 
 ADD sentinel.conf /etc/redis/sentinel.conf
 ADD redis.conf /etc/redis/redis.conf
+ADD redis-startup.sh /usr/local/bin/redis-startup.sh
+RUN chmod 755 /usr/local/bin/redis-startup.sh
 
 # Define mountable directories
-VOLUME ["/data/logs", "/data/db"]
+VOLUME ["/data/logs", "/data/db", "/data/conf"]
 
 # Define working directory
 WORKDIR /data
 
 RUN mkdir -p /data/db
 RUN mkdir -p /data/logs
+RUN mkdir -p /data/conf
 
 # Define entrypoint
-ENTRYPOINT ["redis-sentinel", "/etc/redis/sentinel.conf"]
+CMD ["/bin/bash", "/usr/local/bin/redis-startup.sh"]
 
 # Expose ports
-EXPOSE 26379
+EXPOSE 26379 6379
---
+++ redis-startup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -n "$REDIS_MASTER_SERVER" ]; then
+	redis-server /etc/redis/redis.conf
+fi
+
+if [ -n "$REDIS_CUSTOM_MASTER_SERVER" ]; then
+	redis-server $REDIS_CUSTOM_MASTER_SERVER
+fi
+
+if [ -n "$REDIS_SLAVE_SERVER" ]; then
+	redis-server /etc/redis/redis.conf --slaveof $REDIS_SLAVE_SERVER
+fi
+
+if [ -n "$REDIS_CUSTOM_SLAVE_SERVER" ]; then
+	if [ -n "$REDIS_SLAVE_SERVER_FOR" ]; then
+		redis-server $REDIS_CUSTOM_SLAVE_SERVER --slaveof $REDIS_SLAVE_SERVER_FOR
+	else
+		redis-server $REDIS_CUSTOM_SLAVE_SERVER
+	fi
+fi
+
+if [ -n "$REDIS_SENTNEL_SERVER" ]; then
+	redis-sentinel /etc/redis/sentinel.conf
+fi
+
+if [ -n "$REDIS_CUSTOM_SENTNEL_SERVER" ]; then
+	redis-sentinel $REDIS_CUSTOM_SENTNEL_SERVER 
+fi
+
